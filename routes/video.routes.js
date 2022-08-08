dd99bd9928b176a0355209651f8bc963a8a7ea91
Philip Trachtenberg <philtrachtenberg@gmail.com>
2022-08-08 10:24:56 +0100

    ability to upload videos

--- routes/video.routes.js
+++ routes/video.routes.js
@@ -5,22 +5,29 @@ const Video = require('../models/Video.model.js');
 const fileUploader = require("../config/cloudinary.config");
 
 // require axios
-const { default: axios } = require('axios');
-// const ApiService = require('../services/api.service');
+/* const { default: axios } = require('axios');
+const ApiService = require('../services/api.service'); */
 
 
 
 
 router.get('/videos', (req, res, next) => {
-    res.render('videos')
+    res.render('videos/videos')
 })
 
 router.get('/videos/upload', (req, res, next) => {
-    
+    res.render('videos/upload')
 })
 
 router.post('/videos/upload', (req, res, next) => {
     const {title, level, description, duration, video} = req.body;
+
+    Video.create(req.body)
+    .then((newVideo) => {
+        res.json(newVideo)
+        console.log("new video created: ", newVideo)
+    })
+    .catch(err => console.log(err))
 })
 
 
