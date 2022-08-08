d6e4584ce387086a6a36f7740e4007f041f15674
Philip Trachtenberg <philtrachtenberg@gmail.com>
2022-08-08 12:36:57 +0100

    all CRUD operations work in Postman for videos

--- routes/video.routes.js
+++ routes/video.routes.js
@@ -12,7 +12,11 @@ const ApiService = require('../services/api.service'); */
 
 
 router.get('/videos', (req, res, next) => {
-    res.render('videos/videos')
+    Video.find()
+    .then((video) => {
+        res.render('videos/videos', {video})
+        res.json(video)
+    })
 })
 
 router.get('/videos/upload', (req, res, next) => {
@@ -28,6 +32,52 @@ router.post('/videos/upload', (req, res, next) => {
         console.log("new video created: ", newVideo)
     })
     .catch(err => console.log(err))
+})
+
+router.get("/videos/:videoId", (req, res, next) => {
+    Video.findById(req.params.videoId)
+    .then((oneVideo) => {
+        res.json(oneVideo)
+        res.render('videos/view-video', {oneVideo})
+    })
+})
+
+
+router.get("/videos/:videoId/edit", (req, res, next) => {
+    const { videoId } = req.params.videoId;
+    const { title, level, description, duration, video } = req.body
+
+    Video.findById(req.params.videoId)
+      .then((foundVideo) => {
+        console.log(foundVideo)
+        res.json(foundVideo);
+        res.render('videos/edit-video', { foundVideo })
+      })
+      .catch((err) => console.log(err));
+  });
+  
+  router.post("/videos/:videoId/edit", (req, res, next) => {
+    const { videoId } = req.params.videoId
+    
+    Video.findByIdAndUpdate(req.params.videoId, req.body)
+      .then((updatedVideo) => {
+        res.json(updatedVideo)
+        // res.redirect(`/videos/${videoId}`);
+      })
+      .catch((err) => console.log(err));
+  });
+
+
+
+router.post('/videos/:videoId/delete', (req, res, next) => {
+    let {videoId} = req.params.videoId
+    
+    Video.findByIdAndRemove(req.params.videoId)
+    .then(() => {
+        res.redirect('/videos')
+    })
+    .catch((err) => console.log(err))
+
 })
 
 
